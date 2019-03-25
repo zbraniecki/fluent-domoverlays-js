ae86c8c90d8907f61e73e0228e159b7d94413241
Zibi Braniecki <gandalf@mozilla.com>
2019-03-25 15:43:37 -0700

    Update benchmark to reset value between runs

--- bench/simple.js
+++ bench/simple.js
@@ -1,6 +1,7 @@
 const { Benchmark } = require('benchmark');
 const { JSDOM } = require('jsdom');
 const { translateNode } = require('../src/index');
+const assert = require('assert');
 
 const { document } = (new JSDOM('')).window;
 
@@ -10,18 +11,39 @@ function parseDOM(s) {
   return div;
 }
 
-const dom = parseDOM(`
+const source = `
   <a href="mozilla.org" data-l10n-name="foo"></a>
-`);
-const l10n = parseDOM(`
+`;
+const l10n = `
   This is a simple <a data-l10n-name="foo">example</a>.
-`);
+`;
+const output = `
+  This is a simple <a href="mozilla.org" data-l10n-name="foo">example</a>.
+`;
 
-const bench = new Benchmark('simple case', () => {
-  translateNode(dom, l10n);
-}, {
-  // onCycle: function(event) { console.log(String(event.target)); },
-  onComplete: (event) => { console.log(`ops/sec: ${event.target}`); },
-});
+{
+  // Sanity check
+  const sourceDOM = parseDOM(source);
+  const l10nDOM = parseDOM(l10n);
 
-bench.run({ async: true });
+  translateNode(sourceDOM, l10nDOM);
+  assert.strictEqual(sourceDOM.innerHTML, output);
+}
+
+{
+  let sourceDOM = parseDOM(source);
+  let l10nDOM = parseDOM(l10n);
+
+  const bench = new Benchmark('simple case', () => {
+    translateNode(sourceDOM, l10nDOM);
+  }, {
+    onCycle: () => {
+      // Rest input values
+      sourceDOM = parseDOM(source);
+      l10nDOM = parseDOM(l10n);
+    },
+    onComplete: (event) => { console.log(`ops/sec: ${event.target}`); },
+  });
+
+  bench.run({ async: true });
+}
